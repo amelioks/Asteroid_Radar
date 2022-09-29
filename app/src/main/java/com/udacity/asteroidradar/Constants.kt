8c7003adfdb99a585a649ac375dfa4e528785745
Amalia Oktaviana <amalia.okv01@gmail.com>
2022-09-29 16:56:01 +0200

    Adding Content Description for Talk Back Deleting API KEY from Constant

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "uYpqFd8crRtVVObZ2G3viAwyI0o74rwUCNZTXarv"
+    const val API_KEY = BuildConfig.NASA_API_KEY
 
     const val QUERY_API_KEY = "api_key"
     const val QUERY_START_DATE = "start_date"
